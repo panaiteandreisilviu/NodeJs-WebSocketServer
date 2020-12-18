fc0754d5c3e5a4472ad00252ab215b542686fe92
silviu <andrei.panaite@neurony.ro>
2020-12-18 17:55:25 +0200

    Cors fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser')
 var http = require('http').createServer(app);
 var io = require('socket.io')(http, {
     cors: {
-        origin: "*",
+        origin: "*:*",
         methods: ["GET", "POST"]
     }
 });
