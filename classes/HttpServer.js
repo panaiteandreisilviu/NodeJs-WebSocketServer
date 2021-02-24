2f37ff3e47a0eeba300a2e76930e938a82d28318
silviu <andrei.panaite@neurony.ro>
2021-02-24 10:39:13 +0200

    https fix

--- classes/HttpServer.js
+++ classes/HttpServer.js
@@ -7,20 +7,30 @@ class HttpServer {
 
     constructor() {
 
+        // Get express instance
         this.expressAppInstance = require('express')();
 
+        // Cors Fix
         this.expressAppInstance.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
 
-        this.httpInstance = require('http').createServer(this.expressAppInstance);
+        // HTTPS Certificate
+        let fs = require('fs');
+        let privateKey  = fs.readFileSync('key.pem', 'utf8');
+        let certificate = fs.readFileSync('cert.pem', 'utf8');
+        let credentials = {key: privateKey, cert: certificate};
 
+        // Create server instance
+        this.httpInstance = require('http').createServer(credentials, this.expressAppInstance);
+
+        // CLI Params
         let cliArgsPort = process.argv.slice(3);
-
         let port = parseInt(cliArgsPort) ? parseInt(cliArgsPort) : 10000;
 
+        // Start listening
         this.httpInstance.listen(port, () => {
             console.log('listening on *:' + port);
             //logMsg('SERVER LISTENING ON *:' + port);
